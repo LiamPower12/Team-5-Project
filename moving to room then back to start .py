b98e44ea25ac42e168274fe74e60e89eefe22eb1
LiamPower12 <87702766+LiamPower12@users.noreply.github.com>
2021-07-21 14:49:38 -0230

    Add files via upload

---
+++ moving to room then back to start .py
@@ -0,0 +1,60 @@
+import robot_mac
+r = robot_mac.RobotController()
+r.connect()
+
+def room_num_1():
+    r.left(135)
+    r.forward(360)
+
+room_num_1()
+
+def room_num_2():
+    r.forward(560)
+
+room_num_2()
+
+def room_num_3():
+    r.forward(440)
+    r.rotate_counterclockwise(90)
+    r.forward(1550)
+
+room_num_3()
+
+def room_num_4():
+    r.forward(560)
+    r.rotate_counterclockwise(86)
+    r.forward(835)
+    r.rotate_counterclockwise(84)
+    r.forward(690)
+    r.rotate_clockwise(83)
+    r.forward(700)
+
+room_num_4()
+
+def room_num_5():
+    r.left(1000)
+    r.forward(210)
+    r.rotate_counterclockwise(85)
+    r.forward(450)
+
+room_num_5()
+
+def room_back():
+    r.backward(450)
+    r.left(200)
+    r.backward(900)
+    r.left(650)
+    r.rotate_clockwise(1)
+    r.backward(1000)
+    r.left(900)
+    r.rotate_counterclockwise(6)
+    r.forward(2000)
+    r.rotate_clockwise(45)
+    r.forward(350)
+    r.rotate_clockwise(42)
+    r.forward(1270)
+    r.rotate_counterclockwise(180)
+
+
+
+room_back()
