8d828de3c5823eda8ffaebf0e47240938f877361
sophiawhite1 <scwhite@mun.ca>
2021-07-23 11:14:16 -0230

    updated movements of robot

--- moving.py
+++ moving.py
@@ -1,6 +1,7 @@
 import robot
 r = robot.RobotController()
 r.connect()
+r.take_temperature()
 
 #person1/no_person1 will bring the robot to room two using different routes depending if there is a person
 #to be saved in room 1
@@ -14,15 +15,16 @@ def person1():
     r.rescue_person()
     r.backward(150)
     r.right(700)
-    r.backward(350)
-    r.forward(950)
+    r.backward(250)
+    r.forward(850)
 
 def room_num_1():
     r.left(100)
     r.forward(360)
+    r.read_marker()
     if r.read_marker() == 1:
-        #temp function
         r.left(675)
+        r.take_temperature()
         r.forward(100)
         r.scan_for_people()
         if r.scan_for_people() == False:
@@ -36,89 +38,88 @@ def room_num_1():
 room_num_1()
 
 def room_num_2():
+    r.read_marker()
     if r.read_marker() == 1:
-        #temp function
-        r.left(385)
+        r.left(675)
+        r.take_temperature
         r.forward(150)
+        r.scan_for_fire()
         while r.scan_for_fire() == True:
             r.extinguish_fire()
-        r.right(385)
         r.backward(150)
-    r.forward(560)
+        r.right(675)
 
 
 room_num_2()
 
 def room_num_3():
-    r.forward(440)
+    r.forward(450)
     r.rotate_counterclockwise(90)
-    r.forward(1550)
-    if r.read_marker() == 1:
-        #move into room
-        #temp function
-        #move out of room
-    r.forward(440)
-    r.rotate_counterclockwise(90)
-    r.forward(1550)
+    r.forward(1660)
+    r.read_marker()
+    while r.read_marker() == 1:
+        r.left(150)
+        r.take_temperature
+        r.right(250)
+
 
 room_num_3()
 
+
 def room_num_4():
+    r.forward(600)
+    r.rotate_counterclockwise(90)
+    r.forward(900)
+    r.left(800)
+    r.forward(850)
+    r.read_marker()
     if r.read_marker() == 1:
-        #temp function
-        r.forward(250)
-        r.rotate_counterclockwise(90)
-        r.forward(450)
+        r.forward(300)
+        r.take_temperature
+        r.left(600)
+        r.scan_for_fire()
         while r.scan_for_fire() == True:
             r.extinguish_fire()
-        r.rotate_counterclockwise(180)
-        r.forward(450)
-        r.rotate_clockwise(90)
-        r.forward(250)
-    r.forward(560)
-    r.rotate_counterclockwise(86)
-    r.forward(835)
-    r.rotate_counterclockwise(84)
-    r.forward(690)
-    r.rotate_clockwise(83)
-    r.forward(700)
+        r.right(600)
+        r.backward(325)
 
 room_num_4()
 
-
 def room_num_5():
+    r.left(1475)
+    r.forward(150)
+    r.read_marker()
     if r.read_marker() == 1:
-        #temp function
-        #move robot to person
+        r.forward(300)
+        r.take_temperature()
+        r.left(300)
         r.scan_for_people()
         if r.scan_for_people() == True:
             r.rescue_person()
-        #leave room
-    r.left(1000)
-    r.forward(210)
-    r.rotate_counterclockwise(85)
-    r.forward(450)
+        r.right(300)
+        r.backward(450)
+
 
 
 room_num_5()
 
 
 def room_back():
-    r.backward(450)
-    r.left(200)
-    r.backward(900)
-    r.left(650)
-    r.rotate_clockwise(1)
-    r.backward(1000)
-    r.left(900)
-    r.rotate_counterclockwise(6)
-    r.forward(2000)
-    r.rotate_clockwise(45)
-    r.forward(350)
-    r.rotate_clockwise(42)
-    r.forward(1270)
-    r.rotate_counterclockwise(180)
+    r.right(1475)
+    r.backward(800)
+    r.right(800)
+    r.backward(850)
+    r.left(2300)
+    r.forward(1400)
 
 
 
 room_back()
+
+import matplotlib.pyplot as plt
+from math import sin, cos
+import numpy
+x = [1, 2, 3, 4, 5, 6]
+y = [sin(1), sin(2), sin(3), sin(4), sin(5), sin(6)]
+plt.plot(x, y)
+plt.show()
